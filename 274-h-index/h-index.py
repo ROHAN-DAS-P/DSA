851ede2163f3276ea312384a7793feed7080fca6
ROHAN-DAS-P <98446063+ROHAN-DAS-P@users.noreply.github.com>
2026-02-23 13:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (91.79%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse = True)
+        n = len(citations)
+        idx = 0
+
+        while idx < n and citations[idx] > idx:
+            idx += 1
+        
+        return idx
